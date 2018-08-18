ddce2cb707270e85d3875c995b580b551a716154
Xdvz55a <richardleto@yahoo.com>
2018-08-18 10:11:03 -0400

    Update HelloGianna_tst.py

--- HelloGianna_tst.py
+++ HelloGianna_tst.py
@@ -4,7 +4,7 @@ from HelloGianna import HelloGianna
 class MyTestCase(unittest.TestCase):
   def test_default_greeting_set(self):
     hw = HelloGianna()
-    self.assertEqual(hw.message, 'Hello world!')
+    self.assertEqual(hw.message, 'Hello Bananna!')
 
 if __name__ == '__main__':
   unittest.main()
