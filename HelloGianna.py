9f7e0497f8343a19ffb2a6f3268273faa4b1c0d3
Xdvz55a <richardleto@yahoo.com>
2018-08-18 11:06:30 -0400

    Update HelloGianna.py

--- HelloGianna.py
+++ HelloGianna.py
@@ -1,5 +1,23 @@
-print('Hello Bananna')
+import subprocess
+import os
+import datetime
+import requests
+import json
+import time
+from flask import Flask, request
+from flask_restful import Resource, Api
+from werkzeug import secure_filename
 
-class HelloGianna:
-  def __init__(self):
-    self.message = 'Hello Bananna!'
+app = Flask(__name__)
+api = Api(app)
+
+now = datetime.datetime.now()
+# print (now)
+today = now.strftime("%Y-%m-%d-%H-%M")
+
+@app.route('/hello/', methods=['GET'])
+def upload_file():
+    return 'Hello Gianna'
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0',port='5004')
