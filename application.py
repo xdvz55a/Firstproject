18d596f0416cb16c70dfe968169ac1add14dd1d1
Xdvz55a <richardleto@yahoo.com>
2018-08-22 21:43:46 -0400

    Create application.py

---
+++ application.py
@@ -0,0 +1,23 @@
+import subprocess
+import os
+import datetime
+import requests
+import json
+import time
+from flask import Flask, request
+from flask_restful import Resource, Api
+from werkzeug import secure_filename
+
+application = Flask(__name__)
+api = Api(application)
+
+now = datetime.datetime.now()
+# print (now)
+today = now.strftime("%Y-%m-%d-%H-%M")
+
+@application.route('/hello/', methods=['GET'])
+def upload_file():
+    return 'Hello Gianna Bananna, how are you'
+
+if __name__ == '__main__':
+application.run(host='0.0.0.0',port='5004')
